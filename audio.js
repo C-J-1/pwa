4964ff9c7b2535a6fc1e2f0e4c06353306f09a3d
Christian Jensen <24825148+C-J-1@users.noreply.github.com>
2019-09-10 17:20:08 +1000

    Display Waveform Of Mic Input

--- audio.js
+++ audio.js
@@ -4,12 +4,15 @@ document.getElementById("myBtn").addEventListener("click", function(){
 
       var aContext = new AudioContext();
       var buffSizeRend = 16384;
+      var canvas = document.getElementById('canv');
+      var canvasCtx = canvas.getContext("2d");
 
       var micStream = null,
       gainNode = null,
       scriptProcessNode = null,
       scriptProcessAnalysis = null,
       analyserNode = null;
+
 
       if (!navigator.getUserMedia)
           navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
@@ -58,14 +61,46 @@ document.getElementById("myBtn").addEventListener("click", function(){
           analyserNode.connect(scriptProcessAnalysis);
 
           var buffLength = analyserNode.frequencyBinCount;
-
           var freqArray = new Uint8Array(buffLength);
           var timeArray = new Uint8Array(buffLength);
+
+
+          function draw() {
+            canvasCtx.clearRect(0, 0, 400, 250);
+            var drawVisual = requestAnimationFrame(draw);
+            analyserNode.getByteTimeDomainData(timeArray);
+            canvasCtx.fillStyle = 'rgb(77, 81, 99,0)';
+            canvasCtx.fillRect(0, 0, 400, 250);
+            canvasCtx.lineWidth = 2;
+            canvasCtx.strokeStyle = 'rgb(255, 255, 255)';
+            canvasCtx.beginPath();
+            var sliceWidth = 400 * 1.0 / buffLength;
+            var x = 0;
+
+            for(var i = 0; i < buffLength; i++) {
+
+              var v = timeArray[i] / 128.0 ;
+              var y = v * 250/2;
+
+              if(i === 0) {
+                canvasCtx.moveTo(x, y);
+              } else {
+                canvasCtx.lineTo(x, y);
+              }
+
+              x += sliceWidth;
+            }
+
+            canvasCtx.lineTo(canvas.width, canvas.height/2);
+            canvasCtx.stroke();
+
+          };
+          draw();
+
 
           scriptProcessAnalysis.onaudioprocess = function() {
 
               analyserNode.getByteFrequencyData(freqArray);
-              analyserNode.getByteTimeDomainData(timeArray);
 
               // draw spectrogram
               if (micStream.playbackState == micStream.PLAYING_STATE) {
